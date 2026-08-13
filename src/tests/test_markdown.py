c661bf608386db64d53f9e18a68213640eb1ff5d
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-08-13 18:22:25 -0400

    Added: text to textnodes for all enum properties

--- src/tests/test_markdown.py
+++ src/tests/test_markdown.py
@@ -1,7 +1,7 @@
 import unittest
 from src.markdown.extract_markdown import extract_markdown_images, extract_markdown_links
 from src.nodes.textnode import TextNode, TextType
-from src.markdown.inline_markdown import split_nodes_image, split_nodes_link
+from src.markdown.inline_markdown import split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestMarkdown(unittest.TestCase):
 
@@ -32,3 +32,23 @@ class TestMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_text_to_textnodes(self):
+
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+        new_text = text_to_textnodes(text)
+        expected_output = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+
+        self.assertEqual(new_text, expected_output)
