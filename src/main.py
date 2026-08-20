7910a3fe6deebc383f233d56f53070bf6b369afc
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-08-20 15:11:52 -0400

    Added: Recursive src to dst file and dir copying

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_dir(src: str, dst: str) -> None:
+
+    if not os.path.isdir(dst):
+        os.mkdir(dst)
+
+    for item in os.listdir(src):
+
+        srcfull_path = os.path.join(src, item)
+        dstfull_path = os.path.join(dst, item)
+
+        if os.path.isfile(srcfull_path):
+            print(f"Created File: {srcfull_path}")
+            shutil.copy(srcfull_path, dstfull_path)
+
+        if os.path.isdir(srcfull_path):
+            print(f"Created Directory: {srcfull_path}")
+            copy_dir(srcfull_path, dstfull_path)
 
 def main():
 
-    return TextNode("This is some anchor text", TextType.LINKS, "https://www.boot.dev")
+    if os.path.exists("public"):
+        shutil.rmtree("public")
 
+    copy_dir("static", "public")
 
 if __name__ == "__main__":
 
-    print(main())
+    main()
